2bfb2d8939868a312f684fe2b95cf76b66e1f483
K V N SAI Siddartha <siddarthakrottapalli@gmail.com>
2026-02-25 11:25:28 +0530

    fix: remove terser, use esbuild

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,6 @@ export default defineConfig({
     outDir: 'dist',
     assetsDir: 'assets',
     sourcemap: false,
-    minify: 'terser',
     rollupOptions: {
       output: {
         manualChunks: {
